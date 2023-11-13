d18d9f34c91bcb606796d2c65c685bc5705f8169
billojode <ojodebill@gmail.com>
2023-11-13 13:32:31 +0300

    2

---
+++ conversion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+	
+	int c = 26;
+	float f = (c * 9/5) +32;
+	
+
+	printf("The temperature is %.2f Farenheit\n", f);
+	return(0);
+
+}
---
+++ sum.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+
+	printf("Sum is %d\n", 1+5);
+	return(0);
+}
